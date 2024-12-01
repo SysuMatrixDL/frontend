8b53ff0eb8fcea05245095e18bc5853220a1eb9b
fuuzen <80118151@qq.com>
2024-12-01 14:53:35 +0800

    init repo

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,23 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
-
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+import {
+    type RouteConfig,
+    route,
+    index,
+    layout,
+    prefix,
+  } from "@react-router/dev/routes";
+  
+  export default [
+    index("./home.tsx"),
+    route("about", "./about.tsx"),
+  
+    layout("./auth/layout.tsx", [
+      route("login", "./auth/login.tsx"),
+      route("register", "./auth/register.tsx"),
+    ]),
+  
+    ...prefix("da", [
+      index("./concerts/home.tsx"),
+      route(":city", "./concerts/city.tsx"),
+      route("trending", "./concerts/trending.tsx"),
+    ]),
+  ] satisfies RouteConfig;
